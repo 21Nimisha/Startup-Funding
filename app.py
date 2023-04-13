b7324f3b960f517ec39475a14edd7d377cece842
nimisha singh <nimishasingh.1987@gmail.com>
2023-04-13 21:05:18 +0530

    Added a lot of Functionality based on Startup Project

---
+++ app.py
@@ -0,0 +1,63 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+
+st.set_page_config(layout='wide',page_title='StartUp Analysis')
+
+df = pd.read_csv('startup_cleaned.csv')
+
+
+def load_investor_details(investor):
+    st.title(investor)
+    # load the recent 5 investments of the investor
+    last5_df = df[df['investors'].str.contains(investor)].head()[['date', 'startup', 'vertical', 'city', 'round', 'amount']]
+    st.subheader('Most Recent Investments')
+    st.dataframe(last5_df)
+
+col1, col2 = st.columns(2)
+with col1:
+        # biggest investments
+        big_series = df[df['investors'].str.contains(investors)].groupby('startup')['amount'].sum().sort_values(ascending=False).head()
+        st.subheader('Biggest Investments')
+        fig, ax = plt.subplots()
+        ax.bar(big_series.index,big_series.values)
+
+        st.pyplot(fig)
+
+with col2:
+        verical_series = df[df['investors'].str.contains(investor)].groupby('vertical')['amount'].sum()
+
+        st.subheader('Sectors invested in')
+        fig1, ax1 = plt.subplots()
+        ax1.pie(verical_series,labels=verical_series.index,autopct="%0.01f%%")
+
+        st.pyplot(fig1)
+
+print(df.info())
+
+df['year'] = df['date'].dt.year
+year_series = df[df['investors'].str.contains(investors)].groupby('year')['amount'].sum()
+
+st.subheader('YoY Investment')
+fig2, ax2 = plt.subplots()
+ax2.plot(year_series.index, year_series.values)
+
+st.pyplot(fig2)
+
+st.sidebar.title('Startup Funding Analysis')
+
+option = st.sidebar.selectbox('Select One',['Overall Analysis','StartUp','Investor'])
+
+if option == 'Overall Analysis':
+    load_overall_analysis()
+
+elif option == 'StartUp':
+    st.sidebar.selectbox('Select StartUp',sorted(df['startup'].unique().tolist()))
+    btn1 = st.sidebar.button('Find StartUp Details')
+    st.title('StartUp Analysis')
+else:
+    selected_investor = st.sidebar.selectbox('Select StartUp',sorted(set(df['investors'].str.split(',').sum())))
+    btn2 = st.sidebar.button('Find Investor Details')
+    if btn2:
+        load_investor_details(selected_investor)
+
